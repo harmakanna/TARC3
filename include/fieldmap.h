c09acae3241da92fbe3bbaa7fc8003d79529f426
Jamie Foster <jamiefosterandco@gmail.com>
2026-08-01 07:56:48 +0200

    add frlg NUM_TILES_PER_METATILES

--- include/fieldmap.h
+++ include/fieldmap.h
@@ -15,6 +15,7 @@
 #define MAX_MAP_DATA_SIZE 10240
 
 #define NUM_TILES_PER_METATILE 12
+#define NUM_TILES_PER_METATILE_FRLG 8
 
 // Map coordinates are offset by 7 when using the map
 // buffer because it needs to load sufficient border
