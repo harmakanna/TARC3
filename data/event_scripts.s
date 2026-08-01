1e1a8c40721670c75e2dc7dff46f1e3abb74f100
anna_laptop <harmakanna@gmail.com>
2026-08-01 13:42:56 -0400

    Completed Apartment Building

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1788,3 +1788,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/VirtualCity_Stadium_Arena/scripts.inc"
 
 	.include "data/maps/OutsideWorld_ApartmentBldg1_Floor3/scripts.inc"
+
+	.include "data/maps/OutsideWorld_ApartmentBldg1_YourApartment_Bedroom/scripts.inc"
