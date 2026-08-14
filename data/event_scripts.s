e66973b2d446278a439d594c9a6f980eb4087b8f
Jamie Foster <jamiefosterandco@gmail.com>
2026-08-14 05:22:18 +0200

    add TestingRoomIntro map

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1793,3 +1793,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/OutsideWorld_ApartmentBldg1_Floor3/scripts.inc"
 
 	.include "data/maps/OutsideWorld_ApartmentBldg1_YourApartment_Bedroom/scripts.inc"
+
+	.include "data/maps/VirtualCity_TestingRoomIntro/scripts.inc"
