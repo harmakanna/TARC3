cc4de1a91e922b1fff80b1fc2bfb7a302681ed15
harmakanna <harmakanna@gmail.com>
2026-08-16 11:18:34 -0400

    Added outside world office

--- src/data/tilesets/metatiles.h
+++ src/data/tilesets/metatiles.h
@@ -213,6 +213,9 @@ const u16 gMetatileAttributes_MysteryEventsHouse[] = INCBIN_U16("data/tilesets/s
 const u16 gMetatiles_UnionRoom[] = INCBIN_U16("data/tilesets/secondary/union_room/metatiles.bin");
 const u16 gMetatileAttributes_UnionRoom[] = INCBIN_U16("data/tilesets/secondary/union_room/metatile_attributes.bin");
 
+const u16 gMetatiles_Condominiums[] = INCBIN_U16("data/tilesets/secondary/condominiums_frlg/metatiles.bin");
+const u16 gMetatileAttributes_Condominiums[] = INCBIN_U16("data/tilesets/secondary/condominiums_frlg/metatile_attributes.bin");
+
 #else
 
 
