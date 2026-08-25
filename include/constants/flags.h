594bcddb8e67053ec1006f80e5309b88a62716db
unfolding <unfoldingbat@gmail.com>
2026-08-25 16:31:24 -0700

    start of Act scripting, interrupted by missing data

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1224,10 +1224,10 @@
 
 #define FLAG_DARKANINE_BATTLE_1                                     0x493 // Unused Flag
 #define FLAG_DARKANINE_BATTLE_2                                     0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
-#define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
-#define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
-#define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
+#define FLAG_PROLOGUE_COMPLETE                                      0x495 // Unused Flag
+#define FLAG_ACTI_COMPLETE                                          0x496 // Unused Flag
+#define FLAG_ACTII_COMPLETE                                         0x497 // Unused Flag
+#define FLAG_ACTIII_COMPLETE                                        0x498 // Unused Flag
 #define FLAG_UNUSED_0x499                                           0x499 // Unused Flag
 #define FLAG_UNUSED_0x49A                                           0x49A // Unused Flag
 #define FLAG_UNUSED_0x49B                                           0x49B // Unused Flag
