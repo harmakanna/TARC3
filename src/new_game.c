425024ab3186acac2f315442aee04836ae463bcf
unfolding <unfoldingbat@gmail.com>
2026-08-25 13:48:11 -0700

    attempt 2

--- src/new_game.c
+++ src/new_game.c
@@ -136,14 +136,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-<<<<<<< HEAD
-    if (IS_FRLG)
-        SetWarpDestination(MAP_GROUP(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), MAP_NUM(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), WARP_ID_NONE, 6, 6);
-    else
-        SetWarpDestination(MAP_GROUP(MAP_VIRTUAL_CITY_TESTING_ROOM_INTRO), MAP_NUM(MAP_VIRTUAL_CITY_TESTING_ROOM_INTRO), WARP_ID_NONE, -1, -1);
-=======
     SetWarpDestination(MAP_GROUP(MAP_VIRTUAL_CITY_TESTING_ROOM_INTRO), MAP_NUM(MAP_VIRTUAL_CITY_TESTING_ROOM_INTRO), WARP_ID_NONE, -1, -1);
->>>>>>> d1459f1521102ab00a7b6d7e24e4b34414ed9a31
     WarpIntoMap();
 }
 
