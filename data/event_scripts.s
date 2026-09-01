274eb6076a1c8ee5b361486ca16454147c712f03
unfolding <unfoldingbat@gmail.com>
2026-09-01 03:01:42 -0700

    work maps, research, lotsa scripts, Pokemon sprites (teax3), many fixes

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1814,3 +1814,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/VirtualCity_CommsTower_4F/text.inc"
 
 	.include "data/maps/VirtualCity_Stadium_PreBattleRoom/scripts.inc"
+
+	.include "data/maps/VirtualCity_CommsTower_Research/scripts.inc"
