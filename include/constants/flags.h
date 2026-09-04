27186ad9eeb6e7d8c2995c69d95fa19a60f75972
harmakanna <harmakanna@gmail.com>
2026-09-03 21:34:01 -0400

    Added all Garden NPCs and Ledian sidequest, added missing closemessage to entering VR

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -51,12 +51,12 @@
 
 #else
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_LEDIAN_SPOKE_TO_JESUS    0x20 // Unused Flag
+#define FLAG_LEDIAN_SPOKE_TO_OLD_WOMAN    0x21 // Unused Flag
+#define FLAG_LEDIAN_SPOKE_TO_PLUMP    0x22 // Unused Flag
+#define FLAG_LEDIAN_SPOKE_TO_BABY    0x23 // Unused Flag
+#define FLAG_LEDIAN_SPOKE_TO_MAN    0x24 // Unused Flag
+#define FLAG_IM_BABY         0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
