cf429d3d4fe137ed0cd6c1dea1e1108c1ccb1d00
Jamie Foster <jamiefosterandco@gmail.com>
2026-09-06 00:00:19 +0200

    Add vgc battle support

--- include/constants/battle_frontier.h
+++ include/constants/battle_frontier.h
@@ -18,6 +18,8 @@
 #define FACILITY_LINK_CONTEST      7
 #define FACILITY_UNION_ROOM        8
 #define FACILITY_MULTI_OR_EREADER  9  // Direct Corner multi battles, multi battle with Steven, and e-Reader battles
+#define VGC_BATTLE  10 
+
 
 // Battle Frontier battle modes.
 #define FRONTIER_MODE_SINGLES      0
