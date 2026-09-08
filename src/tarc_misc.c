d39e2ee012770e2165d584fa1020d9fd24b82141
harmakanna <harmakanna@gmail.com>
2026-09-07 21:48:30 -0400

    Added sprite change for workplace, updated Sophie name everywhere, further polish

--- src/tarc_misc.c
+++ src/tarc_misc.c
@@ -1,4 +1,5 @@
 #include "global.h"
+#include "event_data.h"
 
 const u8 gSophieName[] = _("Sophie");
 const u8 gDigitalSophieName[] = _("SoPhi99");
@@ -6,6 +7,19 @@ const u8 gDigitalSophieName[] = _("SoPhi99");
 bool32 IsInVirtualWorld(void)
 {
     return (gSaveBlock1Ptr->location.mapGroup == 0);
+}
+
+
+bool32 IsInWorkplace(void)
+{
+    if (gSaveBlock1Ptr->location.mapNum == MAP_NUM(MAP_VIRTUAL_CITY_WORK_YOUR_OFFICE)
+        ||gSaveBlock1Ptr->location.mapNum == MAP_NUM(MAP_VIRTUAL_CITY_COMMS_TOWER_RESEARCH)
+        ||(gSaveBlock1Ptr->location.mapNum >= MAP_NUM(MAP_VIRTUAL_CITY_COMMS_TOWER_1F) && gSaveBlock1Ptr->location.mapNum <= MAP_NUM(MAP_VIRTUAL_CITY_COMMS_TOWER_TOP_F)))
+    {
+        if(!FlagGet(FLAG_SPOOFING_EXECUTIVE))
+            return TRUE;
+    }
+    return FALSE;
 }
 
 const u8 *GetPlayerName(void)
