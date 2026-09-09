cdbc1dc82b2ac82a6122e7a7dfde6b8a19b999e4
harmakanna <harmakanna@gmail.com>
2026-09-09 17:00:42 -0400

    PreBattleRoom polish, further arena polish (added flags to affect vobjects based on wins), adjusted PreBattleRoom door anim

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -71,8 +71,8 @@
 #define FLAG_SPOKE_TO_RESEARCHER_5    0x31 // Unused Flag
 #define FLAG_SPOKE_TO_RESEARCHER_6    0x32 // Unused Flag
 #define FLAG_SET_RESEARCH_FLOOR       0x33 // Unused Flag
-#define FLAG_ACT_II_WON_RANDO5    0x34 // Unused Flag
-#define FLAG_ACT_II_WON_SILVA_BULLET    0x35 // Unused Flag
+#define FLAG_ACT_II_WON_RANDO3    0x34 // Unused Flag
+#define FLAG_ACT_II_WON_BRODUDE    0x35 // Unused Flag
 #define FLAG_ACT_II_WON_EARL_GRAY    0x36 // Unused Flag
 #define FLAG_UNUSED_0x037    0x37 // Unused Flag
 #define FLAG_UNUSED_0x038    0x38 // Unused Flag
