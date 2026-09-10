f84739a20e92eb683de416f46be9d9e1d348bae4
harmakanna <harmakanna@gmail.com>
2026-09-10 14:02:41 -0400

    Added BW fanfares, adjusted arena and fight music, further polished to endgame, added mugshots

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -76,7 +76,7 @@
 #define FLAG_ACT_II_WON_EARL_GRAY    0x36 // Unused Flag
 #define FLAG_HIDE_LOLA_CONFRONT    0x37 // Unused Flag
 #define FLAG_HIDE_COMMS1F_GUARD    0x38 // Unused Flag
-#define FLAG_UNUSED_0x039    0x39 // Unused Flag
+#define FLAG_IN_ARENA    0x39 // Unused Flag
 #define FLAG_UNUSED_0x03A    0x3A // Unused Flag
 #define FLAG_UNUSED_0x03B    0x3B // Unused Flag
 #define FLAG_UNUSED_0x03C    0x3C // Unused Flag
