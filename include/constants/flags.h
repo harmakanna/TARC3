5895450a906153256c8bcb86a77dddcf70eee4d9
harmakanna <harmakanna@gmail.com>
2026-09-10 11:40:38 -0400

    Polish up to Lola confrontation scene

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -74,7 +74,7 @@
 #define FLAG_ACT_II_WON_RANDO3    0x34 // Unused Flag
 #define FLAG_ACT_II_WON_BRODUDE    0x35 // Unused Flag
 #define FLAG_ACT_II_WON_EARL_GRAY    0x36 // Unused Flag
-#define FLAG_UNUSED_0x037    0x37 // Unused Flag
+#define FLAG_HIDE_LOLA_CONFRONT    0x37 // Unused Flag
 #define FLAG_UNUSED_0x038    0x38 // Unused Flag
 #define FLAG_UNUSED_0x039    0x39 // Unused Flag
 #define FLAG_UNUSED_0x03A    0x3A // Unused Flag
